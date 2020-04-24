2486331a6c0b5fc066dd0995d3b801f9b98198e8
Gabriel Amaral <gabriel.r.c.amaral@gmail.com>
2020-04-24 13:37:34 -0300

    add Desafio2

---
+++ Desafio 2/src/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const fibonacci = () => {return [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377]}
+
+const isFibonnaci = (num) => {return [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377].includes(num)}
+
+module.exports = {
+    fibonacci,
+    isFibonnaci
+}
+
+
